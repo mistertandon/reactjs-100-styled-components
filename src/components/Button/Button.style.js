44882c50fe42f78449a3297fc04ffeed362b0995
mistertandon <enggparveshtandon@gmail.com>
2022-11-04 15:36:13 +0530

    feat: develop basic styled-button component

---
+++ src/components/Button/Button.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    border: 1px solid black;
+    background-color: #c7c7c7;
+    width: 100px;
+    height: 25px;
+`
+
+export default Button;
